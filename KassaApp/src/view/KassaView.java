4a5cd593bbf1adfd31efc7b767e47e51ae5b75be
pdhn <pimdhn@gmail.com>
2019-12-04 15:03:47 +0100

    Story 1, product overview tab is showing all articles. To test you must change hardcoded destination of txt file!

--- KassaApp/src/view/KassaView.java
+++ KassaApp/src/view/KassaView.java
@@ -3,10 +3,14 @@ package view;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;	
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class KassaView {
-	private Stage stage = new Stage();		
+	private Stage stage = new Stage();
+	private static final String CSS_STYLESHEET = "application.css";
 		
 	public KassaView(){			
 		stage.setTitle("KASSA VIEW");
@@ -15,6 +19,8 @@ public class KassaView {
 		stage.setY(20);
 		Group root = new Group();
 		Scene scene = new Scene(root, 750, 500);
+
+		scene.getStylesheets().add(getClass().getResource(CSS_STYLESHEET).toExternalForm()); // Add css sheet
 		BorderPane borderPane = new KassaMainPane();
 		borderPane.prefHeightProperty().bind(scene.heightProperty());
 		borderPane.prefWidthProperty().bind(scene.widthProperty());
--- KassaApp/src/view/panels/ProductOverviewPane.java
+++ KassaApp/src/view/panels/ProductOverviewPane.java
@@ -1,5 +1,6 @@
 package view.panels;
 
+import database.ArtikelTekstLoadSave;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -9,19 +10,70 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import model.Artikel;
 
+import java.util.ArrayList;
+
 
 public class ProductOverviewPane extends GridPane {
-	//private TableView<Product> table;
-	
-	
+	private TableView table;
+	private ArtikelTekstLoadSave artikelen;
+	private static final String FILE_PATH = "C:\\Users\\pimdh\\Documents\\Semester_1_2020\\1_OO Ontwerpen (6)\\Group Werk\\KassaSysteem\\src\\bestanden\\artikel.txt";
+
+
 	public ProductOverviewPane() {
 		this.setPadding(new Insets(5, 5, 5, 5));
         this.setVgap(5);
         this.setHgap(5);
         
 		this.add(new Label("Products:"), 0, 0, 1, 1);
-		
-		
+
+		this.artikelen = new ArtikelTekstLoadSave();
+		ArrayList<Artikel> artikelenInArrayList;
+		artikelenInArrayList = artikelen.load(FILE_PATH);
+
+		TableColumn<String, Artikel> column1 = new TableColumn<>("Artikel Code");
+		column1.setCellValueFactory(new PropertyValueFactory<>("artikelCode"));
+
+		TableColumn<String, Artikel> column2 = new TableColumn<>("Omschrijving");
+		column2.setCellValueFactory(new PropertyValueFactory<>("omschrijving"));
+
+		TableColumn<String, Artikel> column3 = new TableColumn<>("Artikel Groep");
+		column3.setCellValueFactory(new PropertyValueFactory<>("artikelGroep"));
+
+		TableColumn<String, Artikel> column4 = new TableColumn<>("Prijs");
+		column4.setCellValueFactory(new PropertyValueFactory<>("prijs"));
+
+		TableColumn<String, Artikel> column5 = new TableColumn<>("Voorraad");
+		column5.setCellValueFactory(new PropertyValueFactory<>("voorraad"));
+
+		table = new TableView<>();
+
+		table.getColumns().add(column1);
+		table.getColumns().add(column2);
+		table.getColumns().add(column3);
+		table.getColumns().add(column4);
+		table.getColumns().add(column5);
+
+		for (Artikel a: artikelenInArrayList) { // Add each article to table
+			table.getItems().add(a);
+		}
+
+		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+		VBox vBox = new VBox(table);
+
+		this.add(vBox, 0, 0);
+		/*
+		tableView.getItems().add(new Person("John", "Doe"));
+		tableView.getItems().add(new Person("Jane", "Deer"));
+
+		VBox vbox = new VBox(tableView);
+
+		Scene scene = new Scene(vbox);
+
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
+		*/
 	}
 	
 	
